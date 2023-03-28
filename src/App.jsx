37532aff5f3aa85a946336da918e9ac4b92069b6
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-03-28 20:32:02 +0630

    Add two new components and set them up with route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home";
+import GenerateQuotesComponent from "./components/GenerateQuotesComponent";
+import Error from "./components/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="generate" element={<GenerateQuotesComponent />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/GenerateQuotesComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GenerateQuotesComponent() {
+  return <div>GenerateQuotesComponent</div>;
+}
+
+export default GenerateQuotesComponent;
