47261803982d5714a1177289f687d9c6bac370d9
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-03-28 19:25:50 +0630

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="text-center text-lg font-semibold mb-2">
+        Get Inspired ✨
+      </h1>
+      <div className="w-10 h-[0.24rem] bg-violet-800 rounded-lg m-auto"> </div>
+    </header>
+  );
+}
+
+export default Header;
