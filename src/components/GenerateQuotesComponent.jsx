a40a6efbd782208fa2de0ea939a2059ff12f5331
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-04-01 02:14:15 +0630

    Add description and search engine

--- src/components/GenerateQuotesComponent.jsx
+++ src/components/GenerateQuotesComponent.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "./header";
 
 function GenerateQuotesComponent() {
-  return <div>GenerateQuotesComponent</div>;
+  return (
+    <div className="p-5 bg-violet-100 GenerateQuotesComponent">
+      <Header />
+      <Link
+        className=" bg-violet-800 text-white font-semibold py-3 text-center  px-5 absolute right-2 top-3 rounded-lg shadow-lg"
+        to="/"
+      >
+        Home
+      </Link>
+      <p className="my-5 text-base">
+        How to use - Simply select a category of your choice and generate! You
+        can also select how many quotes you want to watch!{" "}
+      </p>
+      <form className="py-4 flex justify-between text-md">
+        <select
+          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
+          name="category"
+          id="category"
+        >
+          <option value="">Category</option>
+        </select>
+        <select
+          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
+          name="amount"
+          id="amount"
+        >
+          <option value="1" selected>
+            1
+          </option>
+        </select>
+        <input
+          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer "
+          type="submit"
+          value="Generate"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default GenerateQuotesComponent;
