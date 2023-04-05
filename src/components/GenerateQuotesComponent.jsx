744ad7a5c8afb6979f0a6047d164ab59202ac1f5
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-04-05 13:15:56 +0630

    Fix widths and styles of buttons

--- src/components/GenerateQuotesComponent.jsx
+++ src/components/GenerateQuotesComponent.jsx
@@ -4,28 +4,28 @@ import Header from "./header";
 
 function GenerateQuotesComponent() {
   return (
-    <div className="p-5 bg-violet-100 GenerateQuotesComponent">
+    <div className="p-5 bg-violet-100 GenerateQuotesComponent min-w-375">
       <Header />
       <Link
-        className=" bg-violet-800 text-white font-semibold py-3 text-center  px-5 absolute right-2 top-3 rounded-lg shadow-lg"
+        className=" bg-violet-800 text-white font-semibold py-2 text-center  px-5 absolute right-2 top-3 rounded-lg shadow-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
         to="/"
       >
         Home
       </Link>
-      <p className="my-5 text-base">
+      <p className="my-5 text-base text-center">
         How to use - Simply select a category of your choice and generate! You
         can also select how many quotes you want to watch!{" "}
       </p>
-      <form className="py-4 flex justify-between text-md">
+      <form className="w-80 m-auto py-4 flex justify-around text-md lg:w-1/2  lg:justify-between">
         <select
-          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
+          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg shadow-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
           name="category"
           id="category"
         >
           <option value="">Category</option>
         </select>
         <select
-          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
+          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg shadow-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
           name="amount"
           id="amount"
         >
@@ -34,7 +34,7 @@ function GenerateQuotesComponent() {
           </option>
         </select>
         <input
-          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer "
+          className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg shadow-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer "
           type="submit"
           value="Generate"
         />
