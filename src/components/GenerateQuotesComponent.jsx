8630965b0012dbc5e0b3300ec4809b89423dea14
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-04-05 15:47:53 +0630

    Add options to choose and styles them

--- src/components/GenerateQuotesComponent.jsx
+++ src/components/GenerateQuotesComponent.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Header from "./header";
 
 function GenerateQuotesComponent() {
+
   return (
     <div className="p-5 bg-violet-100 GenerateQuotesComponent min-w-375">
       <Header />
@@ -22,15 +23,55 @@ function GenerateQuotesComponent() {
           name="category"
           id="category"
         >
-          <option className="bg-violet-500" value="">Category</option>
+          <option value="anxiety">Anxiety</option>
+          <option value="change">Change</option>
+          <option value="choice">Choice</option>
+          <option value="confidence">Confidence</option>
+          <option value="courage" selected>Courage</option>
+          <option value="death">Death</option>
+          <option value="dreams">Dreams</option>
+          <option value="excellence">Excellence</option>
+          <option value="failure">Failure</option>
+          <option value="fairness">Fairness</option>
+          <option value="fear">Fear</option>
+          <option value="forgiveness">Forgiveness</option>
+          <option value="freedom">Freedom</option>
+          <option value="future">Future</option>
+          <option value="happiness">Happiness</option>
+          <option value="inspiration">Inspiration</option>
+          <option value="kindness">Kindness</option>
+          <option value="leadership">Leadership</option>
+          <option value="life">Life</option>
+          <option value="living">Living</option>
+          <option value="love">Love</option>
+          <option value="pain">Pain</option>
+          <option value="past">Past</option>
+          <option value="success">Success</option>
+          <option value="time">Time</option>
+          <option value="today">Today</option>
+          <option value="truth">Truth</option>
+          <option value="work">Work</option>
+          
         </select>
         <select
           className="bg-violet-800 px-4 py-2 text-center text-white font-semibold tracking-wide rounded-lg shadow-lg focus:outline-none hover:bg-violet-900 hover:cursor-pointer"
           name="amount"
           id="amount"
         >
-          <option value="1" selected>
+          <option className="text-left " value="1" selected>
             1
+          </option>
+          <option value="2">
+            2
+          </option>
+          <option value="3">
+            3
+          </option>
+          <option value="4">
+            4
+          </option>
+          <option value="5" >
+            5
           </option>
         </select>
         <input
